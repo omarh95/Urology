a009ca3e98524d37050d5897f5701cb814d91c13
Omar H <omst@Omars-MacBook-Pro.local>
2018-11-22 17:26:56 -0500

    added more rows

--- Urology/ViewController.swift
+++ Urology/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
 
+        //MARK: - Urination Section
         form +++ Section("Time of Urination")
             <<< TimeRow(){ row in
                 row.title = "Time of urination"
@@ -32,36 +34,50 @@ class ViewController: FormViewController {
                     return !((form.rowBy(tag: "didUserHaveBowelMovementRow") as? SwitchRow)?.value ?? false)
                 })
             }
-    
+            //MARK: - Fluid Intake Section
             +++ Section("Fluid Intake")
             <<< TimeRow(){
                 $0.title = "Time"
-                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+                $0.value = Date()
             }
             <<< IntRow(){
                 $0.title = "Amount"
                 $0.placeholder = "Enter Amount Here"
             }
            
+            //MARK: - Accident Section
             +++ Section("Accident")
             <<< SwitchRow("didUserHaveAccidentRow"){
                 $0.title = "Accident?"
             }
             <<< TimeRow(){ row in
                 row.title = "Time of Accident"
+                row.value = Date()
                 row.hidden = Condition.function(["didUserHaveAccidentRow"], { (form) -> Bool in
                     return !((form.rowBy(tag: "didUserHaveAccidentRow") as? SwitchRow)?.value ?? false)
                 })
             }
-//            <<< PickerRow(){
-//                $0.title = "sample"
-//                row.options = ["Urinary", "Stool"]
-//                row.value = "Urinary"
-//                row.hidden = Condition.function(["didUserHaveAccidentRow"], { (form) -> Bool in
-//                    return !((form.rowBy(tag: "didUserHaveAccidentRow") as? SwitchRow)?.value ?? false)
-//                })
-//            }
+            <<< PickerInputRow<String>(){ row in
+                row.title = "Accident Type"
+                row.options = ["Urinary", "Stool"]
+                row.value = "Urinary"
+                row.hidden = Condition.function(["didUserHaveAccidentRow"], { (form) -> Bool in
+                    return !((form.rowBy(tag: "didUserHaveAccidentRow") as? SwitchRow)?.value ?? false)
+                })
+            }
+            <<< IntRow(){ row in
+                row.title = "amount"
+                row.placeholder = "Enter Amount Here"
+            }
+        
+            //MARK: - Other Symptoms Section
+            +++ Section("Other Symptoms")
+            <<< MultipleSelectorRow<String>() { row in
+                row.title = "Other Symptoms"
+                row.options = ["Fatigue", "Pain", "Naseua", "Dizziness"]
+            }
     }
 
+    
 }
 
