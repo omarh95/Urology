6bebe9a1855cc17637bb31b09ea005398fa145ec
Omar H <omst@omars-macbook-pro.local>
2018-11-22 12:57:32 -0500

    Initial Commit

---
+++ Urology/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Urology
+//
+//  Created by Omar H on 11/22/18.
+//  Copyright © 2018 Omar H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
